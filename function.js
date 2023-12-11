8666b70f4f4b36f605fdd0542ce9f3a08ae39bde
Totoro253 <61892539+totoro253@users.noreply.github.com>
2023-12-11 20:33:44 +0100

    working on function to get the focus on to the textinput to enter a word rightaway

--- function.js
+++ function.js
@@ -1,4 +1,6 @@
 document .addEventListener('DOMContentLoaded',function) {
     document.getElementsByTagName('form')[0].onsubmit=function(evt){
         evt.preventDefault();
-    }}
+        window.scrollTo(0,150)
+    }
+    document.getElementById(terminalTextInput).focus();
