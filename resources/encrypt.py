4fdbe3d8d7a584d0ef0b9eaecfa2beef785a785c
Totoro253 <61892539+totoro253@users.noreply.github.com>
2023-12-16 19:52:32 +0100

    removed unused content and added tip

--- resources/encrypt.py
+++
@@ -1,8 +0,0 @@
-password = input("Enter the password you want to encrypt : ")
-encrypted = ''
-x=0
-
-for i in password:
-    encrypted = encrypted + chr(ord(i) + x)
-    x = x + 1
-print(encrypted)
